8cfde183edaee28977ef7cd8cdd4fc960800d463
Simon-Bae <simon.bae825@gmail.com>
2025-02-25 15:58:09 +0900

    feat: amend trainer class
    - _batchify() 메서드 추가 - 입력받은 데이터를 batch_size로 분할하는 기능
    - _train(), _validate() 메서드 추가 - train loop와 valid loop 수행
    - train() 메서드 train 및 valid 수행 중 logging 및 lowest_loss 저장 기능 추가
    - train() 메서드 내부 lowest valid loss의 변화가 없을 시 일찍 train 과정을 종료하는 기능 추가
    - train() 메서드 내부 loss history graph 선택적 출력 기능 추가

--- trainer.py
+++ trainer.py
@@ -2,36 +2,103 @@ import numpy as np
 from copy import deepcopy
 
 import torch
+import torch.nn.functional as func
 
 class Trainer:
-    def __init__(self, model, optimizer, crit):
+    def __init__(self, model, optimizer):
         self.model = model
         self.optimizer = optimizer
-        self.crit = crit
 
         super().__init__()
 
-    def train(self, x, y, config):
-        lowest_loss = np.inf
-        best_model = None
+    @staticmethod
+    def _batchify(x, y, batch_size, random_split=True):
+        if random_split:
+            indices = torch.randperm(x.size(0), device=x.device)
+            x = torch.index_select(x, dim=0, index=indices)
+            y = torch.index_select(y, dim=0, index=indices)
 
-        for idx in range(config.n_epochs):
-            y_hat = self.model(x)
-            loss = self.crit(y_hat, y)
+        x = x.split(batch_size, dim=0)
+        y = y.split(batch_size, dim=0)
 
-            # Initial gradients
+        return x, y
+
+    def _train(self, x, y, config):
+        self.model.train()
+
+        x, y = self._batchify(x, y, config.batch_size)
+        total_loss = 0
+
+        for idx, (x_i, y_i) in enumerate(zip(x, y)):
+            y_hat_i = self.model(x_i)
+            loss_i = func.binary_cross_entropy(y_hat_i, y_i)
+
+            # Initialize gradient
             self.optimizer.zero_grad()
-            loss.backward()
+            loss_i.backward()
 
-            # Gradient descent
+            # Gradient descent one step
             self.optimizer.step()
 
-            if loss < lowest_loss:
-                lowest_loss = loss
-                best_model = deepcopy(self.model.state_dict())
+            total_loss += float(loss_i)
 
-            if (idx + 1) % config.print_interval == 0:
-                print(f"Epoch {idx + 1} : loss={float(loss):.4e}")
+        return total_loss / len(x)
+
+    def _validate(self, x, y, config):
+        self.model.eval()
+
+        with torch.no_grad():
+            x, y = self._batchify(x, y, config.batch_size, random_split=False)
+            total_loss = 0
+
+            for idx, (x_i, y_i) in enumerate(zip(x, y)):
+                y_hat_i = self.model(x_i)
+                loss_i = func.binary_cross_entropy(y_hat_i, y_i)
+
+                total_loss += float(loss_i)
+
+            return total_loss / len(x)
+
+    def train(self, train_data, valid_data, config, to_be_shown=False):
+        lowest_loss = np.inf
+        lowest_epoch = np.inf
+        best_model = None
+        train_history, valid_history = [], []
+
+        for epoch_idx in range(config.n_epochs):
+            train_loss = self._train(train_data[0], train_data[1], config)
+            valid_loss = self._validate(valid_data[0], valid_data[1], config)
+
+            train_history += [train_loss]
+            valid_history += [valid_loss]
+
+            if (epoch_idx + 1) % config.print_interval == 0:
+                print(f"Epoch({epoch_idx + 1}/{config.n_epochs}): train_loss={train_loss:.4e} valid_loss={valid_loss:.4e} lowest_loss={lowest_loss:.4e}")
+
+            if valid_loss <= lowest_loss:
+                lowest_loss = valid_loss
+                lowest_epoch = epoch_idx
+                best_model = deepcopy(self.model.state_dict())
+            else:
+                if config.early_stop > 0 and lowest_epoch + config.early_stop < epoch_idx + 1:
+                    print(f"There is no improvement during last {config.early_stop} epochs.")
+                    break
+
+        if to_be_shown:
+            import matplotlib.pyplot as plt
+
+            plot_from = 2
+            plt.figure(figsize=(20, 10))
+            plt.grid(True)
+            plt.title("Train / Valid Loss History")
+            plt.plot(
+                range(plot_from, len(train_history)), train_history[plot_from:],
+                range(plot_from, len(valid_history)), valid_history[plot_from:],
+            )
+            plt.yscale('log')
+            plt.show()
+
+        print(f"The best validation loss for epoch {lowest_epoch + 1}: {lowest_loss:.4e}")
 
         # Restore best model
         self.model.load_state_dict(best_model)
